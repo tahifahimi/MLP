094522fd3f3984bb42e8231307ee04aac4f994dd
tahere fahimi <t.fahimi1998@gmail.com>
2020-04-25 14:57:16 +0430

    adda the single perceptron but not to find the best values

--- DrawScatter.py
+++ DrawScatter.py
@@ -1,6 +1,5 @@
 import csv
 import matplotlib.pyplot as plt
-
 from SinglePerceptron import SinglePerceptron
 
 
@@ -19,47 +18,49 @@ class drawScatter:
                 else:
                     self.loc.append([float(row[0]), float(row[1]), int(row[2])])
                     line_count += 1
-            # print(f'Processed {line_count} lines.')
 
-    def drawChart(self):
-        print(len(self.loc))
-        print(self.loc[0][0])
-        print(self.loc[0][1])
-        print(type(self.loc[0][0]))
+    def drawChart(self,array,name):
         colors = ["r", "b"]
-        for i in range(len(self.loc)):
-            if self.loc[i][2] == 1:
-                plt.scatter(self.loc[i][0], self.loc[i][1], color=colors[1])
+        print(name,len(array))
+        for i in range(len(array)):
+            if array[i][2] == 1:
+                plt.scatter(array[i][0], array[i][1], color=colors[1])
             else:
-                plt.scatter(self.loc[i][0], self.loc[i][1], color=colors[0])
+                plt.scatter(array[i][0], array[i][1], color=colors[0])
 
         # save scatter
-        plt.savefig('Scatter.png')
+        plt.savefig(name+'.png')
         # plot scatter
-        plt.show()
+        # plt.show()
 
     def trainAndTestData(self):
         train = []
         test = []
         resultOfTest = []
+        numberOfTrainData = int(len(self.loc) * self.learnFactor)
+        print(numberOfTrainData)
+        for i in range(len(self.loc)):
+            if i < numberOfTrainData:
+                train.append(self.loc[i])
+            else:
+                resultOfTest.append(self.loc[i][2])
+                # self.loc[i][2] = 0
+                test.append(self.loc[i])
+        print("number  of test data is ", len(test))
+        self.drawChart(train, "train")
+        # self.drawChart(test, "test")
+        # f = open("testFile.txt", "a")
+        # f.write(str(test[0]))
+        # f.close()
 
-        numberOfTrainData = len(self.loc) * self.learnFactor
-        for i in range(numberOfTrainData):
-            train.append(self.loc[i])
-        for i in range(len(self.loc) - numberOfTrainData):
-            resultOfTest.append(self.loc[i][2])
-            self.loc[i][2] = 0
-            test.append(self.loc[i])
 
         # now call the Single perceptron and pass data
         perceptron = SinglePerceptron(train, test, resultOfTest)
-        perceptron.draw('train')
+        perceptron.learn()
 
 
-
-
-if __name__ == "__main__" :
-    scatter = drawScatter(0.8)  # pass the learning factor to the class
-    scatter.readFile('dataset.csv')
-    scatter.drawChart()
-    scatter.trainAndTestData()
+if __name__ == "__main__":
+    s = drawScatter(0.9)  # pass the learning factor to the class
+    s.readFile('dataset.csv')
+    s.drawChart(s.loc,"main")
+    s.trainAndTestData()
--- SinglePerceptron.py
+++ SinglePerceptron.py
@@ -1,14 +1,8 @@
 from numpy.random import random_sample
-from math import exp
+from math import exp, log
 import random
 import matplotlib.pyplot as plt
 
-def calculateY(param, param1, param2):
-    pass
-
-
-def calculateCost():
-    pass
 
 
 class SinglePerceptron :
@@ -18,51 +12,91 @@ class SinglePerceptron :
         Sets the learning rate
         """
         random.seed(1)
-        self.w = [random.random(), random.random()]
+        self.w = [0.01, 0.1]
         self.train = train
         self.test = test
         self.resultOfTest = resultOfTest
 
         # initialize the  n_epoch and lr
-        self.lr = 0.001
-        self.n_epoch = 100
-        self.bias = 0
+        self.lr = 0.05
+        self.n_epoch = 300
+        self.bias = 1
+
+    def calculateY(self, x0, x1):
+        """ find y of one coordinate
+            W.X+b = x0w0 + x1w1 + b
+            then find its sigmoid! """
+        a = x0 * self.w[0] + x1*self.w[1] + self.bias
+        return float(1)/(1+exp(a*-1))
+
+    def calculateCost(self, yStart, y):
+        """return the value of the cost for just one coordinate"""
+        return yStart*log(y,(10))
+
+    def calculateGradW(self, checker, x0, x1, y0, y):
+        a = x0 * self.w[0] + x1 * self.w[1] + self.bias
+        if checker=="w0":
+            return (float(y0)/y*log(10))*x0*(exp(a*-1)/((1+exp(a*-1))*(1+exp(a*-1))))
+        if checker=="w1":
+            return (float(y0)/y*log(10))*x1*(exp(a*-1)/((1+exp(a*-1))*(1+exp(a*-1))))
+        else:
+            return (float(y0) / y * log(10)) * (exp(a * -1) / ((1 + exp(a * -1)) * (1 + exp(a * -1))))
+
+
 
     def learn(self):
         grad = [0,0]
         bGrad = 0
         """ compute the values of the W and bias for each epoch """
-        for time in  range(self.n_epoch) :
-            grad [0] = 0
+        for time in range(self.n_epoch) :
+            grad[0] = 0
             grad[1] = 0
             bGrad = 0
+            cost = 0
+            modeledY =[]
             for i in range(len(self.train)):
-                # compute y
-                y = calculateY(self.train[i][0],self.train[i][1],self.train[i][2])
-                # compute cost
-                cost = calculateCost()
-                grad[0] += dcost / dW
-                grad[1] += dcost / dW
+                # compute y for the ith data in the train data
+                y = self.calculateY(self.train[i][0], self.train[i][1])
+                modeledY.append(y)
+                # compute cost for the ith data
+                cost += self.calculateCost(self.train[i][2], y)
+                # grad[0] += dcost / dW0 is (y0/yln(10))*S(w0x0+w1x1+bias)(1-S(w0x0+w1x1+bias))* x0
+                # grad[1] += dcost / dW1 is (y0/yln(10))*S(w0x0+w1x1+bias)(1-S(w0x0+w1x1+bias))* x1
+                grad[0] += self.calculateGradW("w0", self.train[i][0], self.train[i][1], self.train[i][2], y)
+                grad[1] += self.calculateGradW("w1", self.train[i][0], self.train[i][1], self.train[i][2], y)
+                bGrad += self.calculateGradW("b", self.train[i][0], self.train[i][1], self.train[i][2], y)
+            cost *= -1
+            grad[0] *= -1
+            grad[1] *= -1
+
+            # draw each generation
+            if time ==99 or time == 0:
+                self.draw("train", time, modeledY)
 
             # assign the grads to the weights
-            self.w[0] = self.w[0] – (self.lr * grad[0]) / len(self.train)
-            self.w[1] = self.w[1] – (self.lr * grad[1]) / len(self.train)
-            self.bias = self.bias – (self.lr * bGrad)/len(self.train)
+            self.w[0] = self.w[0] - (self.lr * grad[0]) / len(self.train)
+            self.w[1] = self.w[1] - (self.lr * grad[1]) / len(self.train)
+            self.bias = self.bias - (self.lr * bGrad)/len(self.train)
 
+            print("gen ",time)
+            print(self.w[0]," ",self.w[1]," ",self.bias)
 
-    def draw(self,typeOfDraw):
+    def draw(self, typeOfDraw, generation, modeledY):
         """ draw data with considering the draw type"""
-        if typeOfDraw == "train" :
+        if typeOfDraw == "train":
             colors = ["r", "b"]
             for i in range(len(self.train)):
-                if self.train[i][2] == 1:
+                if modeledY[i] >= 0.5:
                     plt.scatter(self.train[i][0], self.train[i][1], color=colors[1])
                 else:
                     plt.scatter(self.train[i][0], self.train[i][1], color=colors[0])
         else:
             colors = ["r", "b"]
             for i in range(len(self.test)):
-                if self.test[i][2] == 1:
+                if modeledY[i] >= 0.5:
                     plt.scatter(self.test[i][0], self.test[i][1], color=colors[1])
                 else:
                     plt.scatter(self.test[i][0], self.test[i][1], color=colors[0])
+
+        # plt.show()
+        plt.savefig(str(generation)+'SingleTrain.png')
